324883db22c3f18825246f1bda71ee2d7dfa94e2
Leandro Macato <shadowslayerhero@gmail.com>
2022-08-31 21:27:40 +0800

    init:

---
+++ base/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Room(models.Model):
+    host = models.ForeignKey(User , on_delete=models.SET_NULL , null=True)
+    topic = models.ForeignKey(Topic , on_delete=models.SET_NULL , null=True) # When the topic is deleted room will remain
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True ,blank=True, max_length=200) # Null meaning it can be blank
+    # participants = 
+    updated = models.DateTimeField(auto_now=True) #Automatic DateTimeField | Everytime we save
+    created = models.DateTimeField(auto_now_add=True) #When we first save
+
+    class Meta:
+        ordering = ['-updated', '-created'] # if with "-" it will be reversed
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    user = models.ForeignKey(User , on_delete=models.CASCADE) # A message only has one user
+    room = models.ForeignKey(Room , on_delete=models.CASCADE) # CASCADE Means if room is deleted the Message will also be deleted
+    body = models.TextField(max_length=250)
+    updated = models.DateTimeField(auto_now=True) #Automatic DateTimeField | Everytime we save
+    created = models.DateTimeField(auto_now_add=True) #When we first save
+
+    def __str__(self):
+        return self.body[0:50]
